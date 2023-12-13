da1783a858f49b57bef325078002094bf084dd8e
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-12-12 18:54:12 -0700

    updated api thoughtRoutes and userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,14 @@
 const router = require('express').Router();
 
+const {
+    getAllUsers,
+    createUser,
+    deleteUser,
+    getSingleUser
+} = require('../../controllers/userController');
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
 module.exports = router;
