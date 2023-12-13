b13a8231664e5cfaf6f41900787e6ae0b17b6959
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-12-12 18:44:51 -0700

    added controller methods for user and reactions for controller files

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,4 +34,18 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId }).populate('thoughts').populate('friends');
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+            }
+            res.json(user);
+        }
+        catch (err) {
+            res.status(500).json(err);
+        }
+    },
 };
